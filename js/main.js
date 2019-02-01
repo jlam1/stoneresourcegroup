a00ed72dc7cda539dd0709cd4ceed8ae6d25a977
John Lam <johnlam2478@gmail.com>
2019-02-01 18:58:07 -0500

    stuff

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
 $(document).ready(function () {
 
 	/* NAVBAR */
+	$(window).on("load", function() {
+		var scroll = $(window).scrollTop();
+		var main = $("main").offset().top;
+	
+		if (scroll > main) {
+			$("header.navbar-main").addClass("on-scroll");
+		} else {
+			$("header.navbar-main").removeClass("on-scroll");
+		}
+	});
+
 	$(window).scroll(function() {    
 		var scroll = $(window).scrollTop();
 		var main = $("main").offset().top;
