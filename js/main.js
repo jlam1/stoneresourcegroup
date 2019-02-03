0f9c9ab4417d67f35c50fc4c0114860f64e584fb
John Lam <johnlam2478@gmail.com>
2019-02-03 00:11:06 -0500

    done

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 $(document).ready(function () {
 
-	/* NAVBAR */
-	$(window).on("load", function() {
-		var scroll = $(window).scrollTop();
-		var main = $("main").offset().top;
+	/* NAVBAR ON SCROLL */
+	var scroll, main;
+	$(window).on('load scroll', function() {    
+		scroll = $(window).scrollTop();
+		main = $("main").offset().top;
 	
 		if (scroll > main) {
 			$("header.navbar-main").addClass("on-scroll");
@@ -12,15 +13,25 @@ $(document).ready(function () {
 		}
 	});
 
-	$(window).scroll(function() {    
-		var scroll = $(window).scrollTop();
-		var main = $("main").offset().top;
+	/** AFFILIATIONS RESIZING BLOCKQUOTES BASED ON SIZE OF IMAGE BOX */
+	var affElement, affHeight, affWidth;
+	$(window).on('load resize', function() {
+		affElement = $(".affiliations .aff-list .aff-wrapper .aff:first-child");
+		affHeight = affElement.outerHeight();
+		affWidth = affElement.outerWidth();
 	
-		if (scroll > main) {
-			$("header.navbar-main").addClass("on-scroll");
-		} else {
-			$("header.navbar-main").removeClass("on-scroll");
-		}
+		$(".affiliations .aff-list .aff-wrapper .aff blockquote").css({
+			"min-height": affHeight,
+			"width": (affWidth * 2)
+		});
 	});
 
+	$(".affiliations .aff-list .aff-wrapper .aff").hover(
+		function() {
+			$(".affiliations .aff-list .aff-wrapper .aff").addClass("inactive");
+			$(this).addClass("active").removeClass("inactive");
+		}, function() {
+			$(".affiliations .aff-list .aff-wrapper .aff").removeClass("inactive");
+		}
+	);
 });
